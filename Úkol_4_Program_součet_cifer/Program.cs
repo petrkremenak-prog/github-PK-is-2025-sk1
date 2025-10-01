005bbf9458d063a158e40ace5798f66fe29c1a3c
petrkremenak-prog <petr.kremenak@tul.cz>
2025-10-01 10:22:07 +0200

    Add files via upload

---
+++ Úkol_4_Program_součet_cifer/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Zadejte číslo, u kterého chcete sčítat cifry: ");
+int user_input = int.Parse(Console.ReadLine());
+int sectene_cifry = 0;
+while (!(user_input % 10 == 0))
+    {
+        sectene_cifry++;
+        user_input--;
+    }
+while (!(user_input % 100 == 0))
+{
+    sectene_cifry++;
+    user_input=user_input-10;    
+    }
+while (!(user_input % 1000 == 0))
+{
+    sectene_cifry++;
+    user_input=user_input-100;    
+    }
+while (!(user_input % 10000 == 0))
+{
+    sectene_cifry++;
+    user_input=user_input-1000;    
+    }
+Console.Write("Součet cifer zadaného čísla je: ");
+Console.Write(sectene_cifry);

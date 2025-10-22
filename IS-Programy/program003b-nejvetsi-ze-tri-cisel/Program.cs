4f8b7dc58ff95ce85f3cc74aed545491c7064393
PetrKremenak <vanishak00@gmail.com>
2025-10-22 09:37:45 +0200

    Úkol_5_část2

---
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,51 @@
+﻿string opakovani = "a";
+do
+{
+    int A, B, C;
+
+    Console.Clear();
+    Console.WriteLine("****** Seřažení číselných řad by Petr Křemenák ******");
+    Console.Write("Zadejte první číslo ");
+    while (!int.TryParse(Console.ReadLine(), out A))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu: ");
+    }
+    Console.Write("Zadejte druhé číslo: ");
+    while (!int.TryParse(Console.ReadLine(), out B))
+
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu: ");
+    }
+    Console.Write("Zadejte třetí číslo: ");
+    while (!int.TryParse(Console.ReadLine(), out C))
+    
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu: ");
+    }
+    if (A > B)
+    {
+        if (A > C)
+        {
+            Console.Write("Největší číslo je A = {0}", A);
+        }
+        else
+        {
+            Console.Write("Největší číslo je C = {0}", C);
+        }
+    }
+    else if (B > C)
+    {
+        Console.Write("Největší číslo je B = {0}", B);
+    }
+    else
+    {
+        Console.Write("Největší číslo je C = {0}", C);
+    }
+
+
+
+
+    Console.WriteLine("Konec programu, stiskněte tlačítko a pro opakování ");
+    opakovani = Console.ReadLine();
+
+} while (opakovani == "a");

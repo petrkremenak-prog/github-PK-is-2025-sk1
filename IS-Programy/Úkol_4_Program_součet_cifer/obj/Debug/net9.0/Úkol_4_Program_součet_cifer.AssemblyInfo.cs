870433a0954b02de940cb8857bc6095032bf98a2
PetrKremenak <vanishak00@gmail.com>
2025-10-22 09:03:21 +0200

    Ukol_4_a_5

--- IS-Programy/Úkol_4_Program_součet_cifer/obj/Debug/net9.0/Úkol_4_Program_součet_cifer.AssemblyInfo.cs
+++ IS-Programy/Úkol_4_Program_součet_cifer/obj/Debug/net9.0/Úkol_4_Program_součet_cifer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Úkol_4_Program_součet_cifer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32972fbe52560fe79e50599a9f82d2246ce3c51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622cccb447065442e1d673f703e7f5c5d18b60a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Úkol_4_Program_součet_cifer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Úkol_4_Program_součet_cifer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

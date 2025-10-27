051ee0560b4acdbca78450ce29b26888c0579613
PetrKremenak <vanishak00@gmail.com>
2025-10-27 13:33:33 +0100

    pravouhly_trojuhelnik #7

---
+++ IS-Programy/program004b-pravouhly_trojuhelnik/Program.cs
@@ -0,0 +1,45 @@
+﻿string opakovani = "a";
+do
+{
+    Console.Clear();
+    int výška, šířka;
+    Console.Write("Zadejte Výšku (přirozené číslo): ");
+    while (!int.TryParse(Console.ReadLine(), out výška))
+    {
+        Console.WriteLine("Nezadali jste přirozené číslo. Zadejte znovu: ");
+    }
+    Console.Write("Zadejte Šířku (přirozené číslo): ");
+    while (!int.TryParse(Console.ReadLine(), out šířka))
+    {
+        Console.WriteLine("Nezadali jste přirozené číslo. Zadejte znovu: ");
+    }
+    for (int i = 0; i < výška; i++)
+    {
+        for (int z = 0; z < šířka; z++)
+        {
+            Console.Write(" *");
+            if (z >= i)
+            {
+                break;
+            }
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine("Pro opakování stiskněte tlačítko a: ");
+    opakovani = Console.ReadLine();
+}
+while (opakovani == "a");
